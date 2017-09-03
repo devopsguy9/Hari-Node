3bc7ac59d6dcfc3b28e280cb1dbad237e3227fc9
digitallync <dl5008@digitallyncs-MacBook-Air.local>
2017-09-03 20:53:03 +0530

    app file changed for testing purpose

--- src/app.ts
+++ src/app.ts
@@ -78,7 +78,7 @@ app.all('*', function (req, res, next) {
 
 app.get("/", (request: express.Request, response: express.Response) => {
     response.json({
-        name: "MilkMan On Boarded.. :|"
+        name: "MilkMan Under Development...:) :|"
     })
 });
 
