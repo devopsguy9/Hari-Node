6a2fd47bdde4c43f6b4744072c9c22de1a6e90d0
sdevnoor <31124100+sdevnoor@users.noreply.github.com>
2017-09-03 21:29:41 +0530

    Update subCommunities.ts
    Sharat - Review comments prefixed with DSC

--- src/models/entities/subCommunities.ts
+++ src/models/entities/subCommunities.ts
@@ -4,6 +4,7 @@ import {communities} from './communities';
 import {Dwellings} from './dwellings';
 import {DeliverySchedules} from './deliverySchedules';
 
+// DSC - As discussed in out last meeting, the name of the entity should be singular, not plural
 @Entity("sub_communities")
 export class SubCommunities{
     @PrimaryGeneratedColumn({name:"id"})
@@ -15,9 +16,11 @@ export class SubCommunities{
     })
     name:string;
 
+    // DSC - As discussed, this needs to be typed to a Community entity, not the id
     @JoinTable({name:"community_id"})
     @ManyToMany(type=>communities)
     community_id:number;
+    // DSC the above line has to be community: Community
 
     @Column({name:"active"})
     active:boolean;
